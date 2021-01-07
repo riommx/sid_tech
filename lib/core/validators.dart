d55da3a1e880292707959a6f99b3da1c3251c5c8
riommx <47130561+riommx@users.noreply.github.com>
2021-01-07 00:24:55 -0300

    Rodapes

--- lib/core/validators.dart
+++ lib/core/validators.dart
@@ -1,10 +1,7 @@
 import 'package:dartz/dartz.dart';
-//import 'package:meta/meta.dart';
 import 'package:kt_dart/kt.dart';
 //
 import 'package:sid_tech/core/failures.dart';
-
-// DATETIME
 
 // STRING
 Either<ValueFailure<String>, String> validateStrValueNotNull(
@@ -163,4 +160,19 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
   }
 }
 
-// =====================
+// ******************************************************************
+// *    _____   _   _____      _______   ______    _____   _    _
+// *   / ____| | | |  __ \    |__   __| |  ____|  / ____| | |  | |
+// *  | (___   | | | |  | |      | |    | |__    | |      | |__| |
+// *   \___ \  | | | |  | |      | |    |  __|   | |      |  __  |
+// *   ____) | |_| | |__| |      | |    | |____  | |____  | |  | |
+// *  |_____/  (_) |_____/       |_|    |______|  \_____| |_|  |_|
+// *
+// *  ┈┈┈╭━━╮┈┈┈┈┈┈
+// *  ┈┈╭╯┊◣╰━━━━╮┈┈
+// *  ┈┈┃┊┊┊╱▽▽▽┛┈┈  -< Designed by Sedinir Consentini @ 2021 >-
+// *  ┈┈┃┊┊┊~~~   ┈┈┈┈       -< Rio de Janeiro - Brazil >-
+// *  ━━╯┊┊┊╲△△△┓┈┈
+// *  ┊┊┊┊╭━━━━━━╯┈┈   --->  May the source be with you!  <---
+// * v 1.0
+// ******************************************************************
